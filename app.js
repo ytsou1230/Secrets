d4a7037e2f1af89c0274e7f770d4e785fea603eb
Yulin Tsou <ytsou1230@gmail.com>
2022-07-12 21:32:51 +1200

    Level 3 Hash password added

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+
 
 const app = express();
 
@@ -18,9 +19,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model('User', userSchema);
 
@@ -36,9 +34,13 @@ app.get("/register", function(req,res){
     res.render("register");
 })
 
+app.get("/logout", function(req,res){
+    res.render("home");
+})
+
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser){
         if (err){
@@ -56,7 +58,7 @@ app.post("/login", function(req, res){
 app.post("/register", function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err){
         if (err){
